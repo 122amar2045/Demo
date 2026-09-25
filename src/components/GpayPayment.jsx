033fc2070b7a876d12ca76d2fc9e418b19f063cc
Amar Chauhan <amarc9567@gmail.com>
2026-09-25 18:57:37 +0530

    changes in gpay

--- src/components/GpayPayment.jsx
+++ src/components/GpayPayment.jsx
@@ -3,9 +3,8 @@ import React from 'react'
 function GpayPayment() {
   return (
     <>
-      <div>GpayPayment</div>
       <h1>Gpay Payment from Amar Chauhan</h1>
-      <h2>Hello from Amar Chauhan</h2>
+      <h1>New Gpay Payment</h1>
     </>
   
   )
