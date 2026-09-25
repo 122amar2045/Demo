cfb68e8caa93463113a7ad9a0005c22cf4e9b7a2
Amar Chauhan <amarc9567@gmail.com>
2026-09-25 19:04:04 +0530

    added line in gpay

--- src/components/GpayPayment.jsx
+++ src/components/GpayPayment.jsx
@@ -4,6 +4,9 @@ function GpayPayment() {
   return (
     <>
       <h1>Gpay Payment from Amar Chauhan</h1>
+      <h2>Hello from Amar Chauhan</h2>
+      <h4>Gpay Payment from Amar Chauhan</h4>
+      <p>This is a paragraph from Amar Chauhan</p>
     </>
   
   )
